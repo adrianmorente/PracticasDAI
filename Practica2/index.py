651ec5e536b63c1ce45e852da65fd159759cd2af
Adrian Morente <adrian95morente@gmail.com>
2017-10-17 09:42:53 +0200

    salvando cosas de la semana anterior

--- Practica2/index.py
+++ Practica2/index.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Welcome to the index!'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, world!'
+
+@app.route('/css')
+def get_image():
+    return ('static', filename='style.css')
