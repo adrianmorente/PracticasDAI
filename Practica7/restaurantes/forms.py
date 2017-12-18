186cd60eb974be3af2f0d440219c062991f1e37b
Adrian Morente <adrian95morente@gmail.com>
2017-12-18 15:09:36 +0100

    añadido form para crear restaurante en BD

--- Practica7/restaurantes/forms.py
+++ Practica7/restaurantes/forms.py
@@ -7,20 +7,86 @@ def valida_codigo_postal(value):
     if value < 10000 or value > 99999:
         raise ValidationError('%s no parece ser un código postal' % value)
 
-def valida_edificio(value):
+def valida_numero_edificio(value):
     if value < 1 or value > 5000:
         raise ValidationError('Tan solo existen números de edificio entre 0 y 5000')
 
-
 class RestauranteForm(forms.Form):
-    name = forms.CharField(label='Nombre', max_length=60, strip=True,
-        widget=forms.TextInput(
-            attrs={'class':'form-control text-muted col-md-4',
-            'size':30,
-            'placeholder':'el nombre que sea',
-        }),
-        validators=[valida_nombre])
-    cuisine = forms.CharField(label='Tipo de cocina', max_length=80, required=True)
-    street = forms.CharField(label='Calle', max_length=100, required=True)
-    building = forms.IntegerField(label='Edificio', max_length=4, required=True, validators=[valida_numero_edificio])
-    zipcode = forms.IntegerField(label='Código Postal', required=True, validators=[valida_codigo_postal])
+    name = forms.CharField(
+        label = 'Nombre',
+        max_length = 60,
+        strip = True,
+        widget = forms.TextInput(
+            attrs = {
+                'class' : 'form-control col-md-4',
+                'size' : 30,
+                'placeholder' : 'Casa Morente'
+            }
+        ),
+        required = True
+    )
+
+    borough = forms.CharField(
+        label = 'Barrio',
+        max_length = 80,
+        widget = forms.TextInput(
+            attrs = {
+                'class' : 'form-control col-md-4',
+                'size' : 30,
+                'placeholder' : 'Maracena'
+            }
+        ),
+        required = True
+    )
+
+    cuisine = forms.CharField(
+        label = 'Tipo de cocina',
+        max_length = 80,
+        widget = forms.TextInput(
+            attrs = {
+                'class' : 'form-control col-md-4',
+                'size' : 30,
+                'placeholder' : 'Comida basura'
+            }
+        ),
+        required = True
+    )
+
+    street = forms.CharField(
+        label = 'Calle',
+        max_length = 100,
+        widget = forms.TextInput(
+            attrs = {
+                'class' : 'form-control col-md-4',
+                'size' : 30,
+                'placeholder' : 'Gran Vía'
+            }
+        ),
+        required = True
+    )
+
+    building = forms.IntegerField(
+        label = 'Edificio',
+        required = True,
+        widget = forms.TextInput(
+            attrs = {
+                'class' : 'form-control col-md-4',
+                'size' : 30,
+                'placeholder' : '1'
+            }
+        ),
+        validators = [valida_numero_edificio]
+    )
+
+    zipcode = forms.IntegerField(
+        label = 'Código Postal',
+        required = True,
+        widget = forms.TextInput(
+            attrs = {
+                'class' : 'form-control col-md-4',
+                'size' : 30,
+                'placeholder' : '18001'
+            }
+        ),
+        validators = [valida_codigo_postal]
+    )
