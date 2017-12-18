073fafa42480d70b8adedeb8efcf000a359551d9
Adrian Morente <adrian95morente@gmail.com>
2017-12-18 15:10:05 +0100

    creando y mostrando forms

--- Practica7/restaurantes/views.py
+++ Practica7/restaurantes/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import restaurantes
 from bson.json_util import dumps
+from .forms import RestauranteForm
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, 'restaurantes/index.html', {})
@@ -11,7 +13,13 @@ def register(request):
     return render(request, 'restaurantes/register.html', {})
 
 def restaurants(request):
-    return render(request, 'restaurantes/restaurants.html', {})
+    form = RestauranteForm()
+    if request.method == 'POST':
+        form = RestauranteForm(request.POST)
+        if form.is_valid():
+            datos = form.cleaned_data
+            return redirect('/restaurants')
+    return render(request, 'restaurantes/restaurants.html', { 'form' : form })
 
 def contact(request):
     return render(request, 'restaurantes/contact.html', {})
@@ -33,3 +41,22 @@ def search_ajax(request):
     page_py = int(request.GET.get('page_py', 1))
     query = restaurantes.find({ data_field : field_content }).skip(page_py*10).limit(10)
     return HttpResponse(dumps(query))
+
+def add_restaurant(request):
+    form = RestauranteForm()
+    if request.method == 'POST':
+        form = RestauranteForm(request.POST)
+        if form.is_valid():
+            datos = form.cleaned_data
+            restaurantes.insert({
+             	"address" : {
+             		"building" : datos['building'],
+             		"street" : datos['street'],
+             		"zipcode" : datos['zipcode']
+             	},
+             	"borough" : datos['borough'],
+             	"cuisine" : datos['cuisine'],
+            	"name" : datos['name'],
+            })
+            return redirect('/restaurants')
+    return render(request, 'restaurantes/restaurants.html', { 'form' : form })
